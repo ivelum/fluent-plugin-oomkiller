a15a6232a711353bcefd049a2b0bd82d7d0c32fb
Ilnur Ibragimov <iceone@yandex.ru>
2017-02-01 15:05:12 +0500

    Check for existing time key in record

--- fluent-plugin-oomkiller.gemspec
+++ fluent-plugin-oomkiller.gemspec
@@ -2,10 +2,10 @@
 
 Gem::Specification.new do |spec|
   spec.name          = "fluent-plugin-oomkiller"
-  spec.version       = "0.0.1"
+  spec.version       = "0.0.2"
   spec.authors       = ["Megumi Nakamura"]
   spec.email         = ["nakamegu@gmail.com"]
-  
+
   spec.summary       = %q{Fluentd plugin to get oom killer log.}
   spec.description   = %q{Fluentd plugin to get oom killer log from system message.}
   spec.homepage      = "https://github.com/nakamegu/fluent-plugin-oomkiller"
@@ -18,7 +18,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.8"
   spec.add_development_dependency "rake", "~> 10.0"
-  
+
   spec.add_development_dependency "fluentd"
   spec.add_runtime_dependency "fluentd"
 end
--- lib/fluent/plugin/out_oomkiller.rb
+++ lib/fluent/plugin/out_oomkiller.rb
@@ -1,8 +1,8 @@
 class Fluent::OomKillerOutput < Fluent::Output
   Fluent::Plugin.register_output('oomkiller', self)
-  
+
   include Fluent::HandleTagNameMixin
-  
+
   def configure(conf)
     super
     @logs = {}
@@ -26,7 +26,7 @@ class Fluent::OomKillerOutput < Fluent::Output
       make_record_block(tag, time, record)
     }
   end
-  
+
   def make_record_block(tag, time, record)
     record.each do |key, value|
       if @is_oomlog then
@@ -41,16 +41,23 @@ class Fluent::OomKillerOutput < Fluent::Output
       end
     end
   end
-  
+
   REGEX1 = /^(\S+\s+\d+\s\d+:\d+:\d+)\s.+/
   REGEX2 = /Killed process (\d+),\s+UID\s+(\d+),\s+\((\S+)\)\s+total-vm:(\d+)kB,\s+anon-rss:(\d+)kB,\s+file-rss:(\d+)kB/
   def parse_record_block(tag, time)
     record = {}
-    
-    @logs[:"#{tag}"][0] =~ REGEX1
-    datetime = Time.parse($1)
-    time = datetime.to_i if datetime
-    
+
+    if record.has_key?('time') and record['time']
+      datetime = Time.parse(record['time'])
+      time = datetime.to_i if datetime
+    else
+      @logs[:"#{tag}"][0] =~ REGEX1
+      if $1
+        datetime = Time.parse($1)
+        time = datetime.to_i if datetime
+      end
+    end
+
     @logs[:"#{tag}"][-1] =~ REGEX2
     record['pid'] = $1.to_i
     record['uid'] = $2.to_i
@@ -58,15 +65,15 @@ class Fluent::OomKillerOutput < Fluent::Output
     record['total_vm_kb'] = $4.to_i
     record['anon_rss_kb'] = $5.to_i
     record['file_rss_kb'] = $6.to_i
-    
+
     record['raw'] = @logs[:"#{tag}"].map {|m| m.strip}.join("\n")
-    
+
     flush_emit(tag, time, record)
   end
 
   def flush_emit(tag, time, record)
     @logs[:"#{tag}"].clear
-      
+
     if !@remove_tag_prefix && !@remove_tag_suffix && !@add_tag_prefix && !@add_tag_suffix
       Fluent::Engine.emit(tag, time, record)
 
@@ -79,6 +86,6 @@ class Fluent::OomKillerOutput < Fluent::Output
         $log.warn "Can not emit message because the tag has not changed. Dropped record #{record}"
       end
     end
-    
+
   end
 end
